5826b33e538df7e02677e2d002a570918f61ed50
Ala123Sobhan <ala123sobhan@gmail.com>
2021-07-18 19:55:22 -0400

    handling double click, alert box with chai assertions

--- test/specs/functionalScenarios.js
+++ test/specs/functionalScenarios.js
@@ -1,5 +1,7 @@
+const chaiexpect = require("chai").expect;
+
 describe("JS functional testing", () => {
-  it("scrolling and mouse over", () => {
+  xit("scrolling and mouse over", () => {
     browser.url("https://www.rahulshettyacademy.com/AutomationPractice/");
     $("#mousehover").scrollIntoView();
     //browser.pause(2000);
@@ -7,5 +9,21 @@ describe("JS functional testing", () => {
     //  browser.pause(2000);
     $("=Top").click();
     browser.pause(3000);
+  });
+
+  it("double click alert box handling", () => {
+    browser.url("http://only-testing-blog.blogspot.com/");
+    $("button").scrollIntoView();
+    browser.pause(2000);
+    $("button").doubleClick();
+
+    // console.log(browser.isAlertOpen());
+    chaiexpect(browser.isAlertOpen()).to.be.true;
+    // console.log(browser.getAlertText());
+    chaiexpect(browser.getAlertText()).to.equal(
+      "Press 'OK' or 'Cancel' button!"
+    );
+    browser.acceptAlert();
+    // browser.dismissAlert()
   });
 });
