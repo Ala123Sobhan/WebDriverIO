f8198d1afe8e25a5417275c6cc3de8d78cf78b5e
Ala123Sobhan <ala123sobhan@gmail.com>
2021-07-17 21:21:40 -0400

    handle pop ups with synchronization methods in wdio

--- test/specs/firstTest.js
+++ test/specs/firstTest.js
@@ -1,7 +1,7 @@
 describe("Ecommerce App", () => {
-  it("Login page title", () => {
+  xit("Login fail page title", () => {
     browser.maximizeWindow();
-    
+
     browser.url("https://www.rahulshettyacademy.com/loginpagePractise/#");
     console.log(browser.getTitle());
     browser.pause(3000);
@@ -14,8 +14,8 @@ describe("Ecommerce App", () => {
     //browser.pause(3000);
     signIn.click();
 
-    const msg = $(".alert-danger").getText();
-    console.log("The error msg: " + msg);
+    //const msg = $(".alert-danger").getText();
+    //console.log("The error msg: " + msg);
 
     //browser.waitUntil(condition, { timeout, timeoutMsg, interval })
 
@@ -23,5 +23,29 @@ describe("Ecommerce App", () => {
       () => $("#signInBtn").getAttribute("value") === "Sign In",
       { timeout: 4000, timeoutMsg: "Can get the error msg!" }
     );
+    const msg = $(".alert-danger").getText();
+    console.log("The error msg: " + msg);
+
+    const loginMsg = $("p.text-white");
+    expect(loginMsg).toHaveTextContaining(
+      "(username is rahulshettyacademy and Password is learning)"
+    );
+  });
+
+  xit("Login success page title", () => {
+    browser.maximizeWindow();
+    browser.url("https://www.rahulshettyacademy.com/loginpagePractise/#");
+    browser.pause(3000);
+    expect(browser).toHaveTitleContaining("Rahul Shetty Academy");
+    $("input[id = 'username']").setValue("rahulshettyacademy");
+    const pass = $("//input[@type = 'password']");
+    pass.setValue("learning");
+    const signIn = $("#signInBtn");
+    signIn.click();
+    const chkBtn = $("*=Checkout");
+    //chkBtn.click();
+    chkBtn.waitForExist();
+    chkBtn.click();
+    expect(browser).toHaveTitleContaining("ProtoCommerce");
   });
 });
--- test/specs/uiControls.js
+++ test/specs/uiControls.js
@@ -10,8 +10,25 @@ describe("Ecommerce App", () => {
 
     radioBtn[1].$(".radiotextsty").click();
 
+    const modal = $(".modal-content");
+    modal.waitForDisplayed();
+
+    $("#cancelBtn").click();
+    const usBtn = radioBtn[1].$(".radiotextsty");
+
+   // browser.pause(1000);
+    console.log(usBtn.isSelected());
+
     // const signIn = $("#signInBtn");
     //signIn.click();
-    browser.pause(3000);
+    //browser.pause(3000);
+
+    radioBtn[1].$(".radiotextsty").click();
+    modal.waitForDisplayed();
+    $("#okayBtn").click()
+    console.log(usBtn.isSelected())
+
+    radioBtn[0].$(".radiotextsty").click();
+    expect(modal).not.toBeDisplayed();
   });
 });
